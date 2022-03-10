c23dbfa5e9dce56975c52a7cb55b06ba0f917fdc
Nate Lapinski <nate.lapinski@gmail.com>
2022-03-09 17:29:02 -0800

    aaa

--- make_docs.bat
+++ make_docs.bat
@@ -1,5 +1,5 @@
 python.exe .\py2md.py --sourcedir .\d2mem\ --docfile d2mem.md --projectname d2mem --codelinks
-python -m doc2md -a -t 'd2mem' 'd2mem' > docs_alt.md
+python -m doc2md -a -t 'd2mem' 'd2mem' > d2mem.alt.md
 git add .\d2mem.alt.md
 git add .\d2mem.md
 git commit -m "documentation update"
