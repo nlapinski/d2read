4abe85b93c7004e1d186387419cf64c808e80c47
Nate Lapinski <nate.lapinski@gmail.com>
2022-03-10 10:24:28 -0800

    update

--- make_docs.bat
+++ make_docs.bat
@@ -1,5 +1,6 @@
 pdoc3 --output-dir=.\docs --html --template-dir .\templates\ --force .\d2read\  
 xcopy /s .\docs\d2read\* .\docs\ /y
 git add .\docs\*
+
 git commit -m "documentation update"
 git push
