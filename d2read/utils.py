acba71f24809d5381737bdcb27a50f826ba9dbb8
Nate Lapinski <nate.lapinski@gmail.com>
2022-03-14 13:01:04 -0700

    tidy up utils

--- d2read/utils.py
+++ d2read/utils.py
@@ -1,39 +1,38 @@
 '''utils - some misc helper functions'''
-
-import struct
-
-
-from colored import fg, bg, attr
 import os
+import struct
+from colored import fg, bg, attr
 os.system('color')
 
-#COLORS
 status=242
+''' loot log colors '''
 warn=202
+''' loot log colors '''
 good=123
+''' loot log colors '''
 purp=99
-
-#color globals
+''' loot log colors '''
 combat_color = 160
+''' combat log colors '''
 loot_color = 208
+''' loot log colors '''
 traverse_color = 53
+''' loot log colors '''
 manager_color = 240
+''' loot log colors '''
 traverse_color = 125
+''' loot log colors '''
 sensing_color = 117
+''' loot log colors '''
 note_color = 109
+''' loot log colors '''
 mem_color = 57
+''' loot log colors '''
 offset_color = 128
+''' loot log colors '''
 important_color = 87
+''' loot log colors '''
 
-
-'''
-def closest_node(node, nodes):
-    return nodes[cdist([node], nodes).argmin()]
-
-def closest_node_index(node, nodes):
-    closest_index = distance.cdist([node], nodes).argmin()
-    return closest_index
-'''
 def log_color(text, fg_color=242,bg_color=0,target="",fg2_color=242,bg2_color=0):
     out_text =  fg(fg_color)+bg(bg_color)+text+attr('reset')
     if target != "":
@@ -41,18 +40,3 @@ def log_color(text, fg_color=242,bg_color=0,target="",fg2_color=242,bg2_color=0)
         print(fg(235)+':: '+out_text+" "+target_text)
     else:
         print(fg(235)+':: '+out_text)
-'''
-def close_down_d2():
-    subprocess.call(["taskkill","/F","/IM","D2R.exe"], stderr=subprocess.DEVNULL)
-
-def find_d2r_window() -> tuple[int, int]:
-    if os.name == 'nt':
-        window_list = []
-        EnumWindows(lambda w, l: l.append((w, *GetWindowThreadProcessId(w))), window_list)
-        for (hwnd, _, process_id) in window_list:
-            if psutil.Process(process_id).name() =="D2R.exe":
-                left, top, right, bottom = GetClientRect(hwnd)
-                (left, top), (right, bottom) = ClientToScreen(hwnd, (left, top)), ClientToScreen(hwnd, (right, bottom))
-                return (left, top)
-    return None
-'''
